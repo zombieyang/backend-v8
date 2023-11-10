1f4c507c40cb69ef7527698baa5964af38b263dc
僵尸浩 <348063288@qq.com>
2023-11-10 17:57:07 +0800

    Update rep1.js

--- node-script/rep1.js
+++ node-script/rep1.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 var lines = fs.readFileSync(process.argv[2], 'utf-8').split(/[\n\r]/);
 
 for(var i = 0; i < lines.length; i++) {
-    lines[i] = lines[i].replace('"/wd4312"', '"/wd4834"');
+    lines[i] = lines[i].replace('"/wd4312"', '"/wd4312", "/wd4834"');
 }
         
 fs.writeFileSync(process.argv[2], lines.join('\n'));
